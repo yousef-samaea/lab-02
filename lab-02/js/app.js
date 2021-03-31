198e234f289df437e6a2eab1fa3ddb9a6c964fa5
yousef abea allah atyyeh samara <yousef.aa.samara@gmail.com>
2021-03-31 13:00:00 +0300

    edit

--- lab-02/js/app.js
+++ lab-02/js/app.js
@@ -1,55 +1,61 @@
-'use strict';
-let allImage = [];
-let keywords = [];
-function findKeywords(item) {
-  if (keywords.length === 0) { keywords.push(item.keyword); }
-  let matched = false;
-  for (let i = 0; i < keywords.length; i++) {
-    if (item.keyword === keywords[i]) {
-      matched = true;
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
+'user strict';
+let keywordsArray = [];
+let hornsArray = [];
+$.ajax('./data/page-1.json').then(unicornArray => {
+  // console.log(unicornArray);
+  unicornArray.forEach(unicornData => {
+    // console.log(unicornData);
+    let newUnicorn = new Unicorn(unicornData);
+    newUnicorn.renderData();
+    if (!keywordsArray.includes(unicornData.keyword)) {
+      keywordsArray.push(unicornData.keyword);
     }
-  } if (!matched) { keywords.push(item.keyword); }
-}
-
-function ImgData(item) {
-  this.image_url = item.image_url;
-  this.title = item.title;
-  this.description = item.description;
-  this.keyword = item.keyword;
-  this.horns = item.horns;
-
-}
-ImgData.prototype.render = function () {
-  let photoTemplate = $('#photo-template').clone();
-  photoTemplate.removeAttr('id');
-  photoTemplate.addClass(this.keyword);
-  photoTemplate.html(`<h2>${this.title}</h2>
-        <img src='${this.image_url}' alt='${this.keyword}'>
-        <p>${this.description}</p>`);
-  $('main').append(photoTemplate);
-}
-
-$.get('data/page-1.json').then(data => {
-  data.forEach(item => {
-    let imgData = new ImgData(item);
-    imgData.render();
-    allImage.push(imgData);
-    findKeywords(item);
-  })
-  console.log('lenght from inside : '+ keywords.length);
-  for (let i = 0; i < keywords.length; i++) { // add keywords as options in the selection form.
-    let keyOption = $(`<option value='${keywords[i]}'>${keywords[i]}</option>`);
-    $('#imgFilter').append(keyOption);
-  }
-  $('#photo-template').hide();
+    if (!hornsArray.includes(unicornData.horns)) {
+      hornsArray.push(unicornData.horns);
+    }
+    // console.log(newUnicorn);
+    // console.log(newUnicorn.description);
+  });
+  $('.displayedImage').first().remove();
+  renderOptions();
 });
-//////////////// selection and filter.
-console.log('lenght from outside : '+ keywords.length);
-$('#imgFilter').on('change', function(event){
-  let selected=this.options[this.selectedIndex].value;
-  console.log(selected);
-  $('.imgInfo').hide();
-  $(`.${selected}`).show();
-  if(this.selectedIndex===0){$('.imgInfo').show();$('#photo-template').hide();}
-
+// console.log(keywordsArray);
+// console.log(hornsArray);
+function Unicorn(uniData) {
+  this.image_url = uniData.image_url;
+  this.title = uniData.title;
+  this.description = uniData.description;
+  this.keyword = uniData.keyword;
+  this.horns = uniData.horns;
+  Unicorn.allUnicorn.push(this);
+}
+Unicorn.allUnicorn = [];
+Unicorn.prototype.renderData = function () {
+  let divClone = $('.displayedImage').first().clone();
+  divClone.find('h2').text(this.title);
+  divClone.attr('class', `displayedImage ${this.keyword}`);
+  divClone.find('img').attr('src', `${this.image_url}`);
+  divClone.find('p').text(this.description);
+  $('main').append(divClone);
+};
+const renderOptions = () => {
+  keywordsArray.forEach(keyword => {
+    let optionClone = $('option').first().clone();
+    optionClone.html(keyword);
+    optionClone.attr('value', keyword);
+    $('select').append(optionClone);
+    // console.log(keyword);
+  });
+  // console.log(keywordsArray);
+};
+$('select').change(function () {
+  let val = $(this).children('option:selected').val();
+  if (val !== 'default') {
+    $('div').hide();
+    // console.log(val);
+    $(`.${val}`).show();
+  }
+  else $('.displayedImage').show();
 });
